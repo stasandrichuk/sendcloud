a83e1c1d07e2f001f8cd2786e2285b6b18d18ec6
n2me <n2me@alex.local>
2017-04-06 00:31:04 +0300

    added price field and weight

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -8,7 +8,7 @@ class ParcelsController < ApplicationController
     begin
       shipment_address = Sendcloud::ShipmentAddress.new(params[:address], params[:city], params[:postal_code], params[:country])
       shipment = {id: params[:shipping_methods], options: []}
-      method_params = {telephone: params[:telephone], email: params[:email], house_number: params[:house_number]}
+      method_params = {telephone: params[:telephone], email: params[:email], house_number: params[:house_number], weight: params[:weight]}
 
       puts shipment_address.inspect
       puts shipment
